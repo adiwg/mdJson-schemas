ca66ec3b65d16ece5397a2eedcbc330a3acee03a
Josh Bradley <jbradley@arcticlcc.org>
2014-05-06 11:09:30 -0800

    Remove files.

--- lib/adiwg/json_schemas/json_schemas.rb
+++
@@ -1,9 +0,0 @@
-require "adiwg/json/schemas/version"
-
-module Adiwg
-  module JSON
-    module Schemas
-        autoload :Utils, 'adiwg/json/schemas/utils'
-    end
-  end
-end
--- lib/adiwg/json_schemas/schemas/version.rb
+++
@@ -1,8 +0,0 @@
-module Adiwg
-    module JSON
-        module Schemas
-            # Current schema version number
-            VERSION = "0.4.0"
-        end
-    end
-end
