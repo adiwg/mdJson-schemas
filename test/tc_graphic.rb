a2eddaf6fc6fd5f212fd6fefb473bab795e834aa
Josh Bradley <jbradley@arcticlcc.org>
2016-12-30 16:48:14 -0900

    Rename graphicOverview to graphic and update
    See #45

---
+++ test/tc_graphic.rb
@@ -0,0 +1,8 @@
+require 'helper.rb'
+
+class TestGraphic < TestHelper
+    def test_graphic
+        errors = JSON::Validator.fully_validate('graphic.json', @@example + 'graphic.json', strict: @@strict, list: false)
+        assert(errors.empty?, errors.join("\n"))
+    end
+end
