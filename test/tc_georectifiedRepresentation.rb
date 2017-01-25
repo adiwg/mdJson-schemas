72bd49bdfdc23ecd3704d14121594f6fdc628c05
Josh Bradley <jbradley@arcticlcc.org>
2017-01-24 19:49:50 -0800

    Support georectifiedRepresentation
    See #79

---
+++ test/tc_georectifiedRepresentation.rb
@@ -0,0 +1,9 @@
+require 'helper.rb'
+
+class TestGeorectifiedRepresentation < TestHelper
+    def test_georectifiedRepresentation
+        errors = JSON::Validator.fully_validate('georectifiedRepresentation.json', @@example + 'georectifiedRepresentation.json', strict: @@strict, list: true)
+        assert(errors.empty?, errors.join("
+"))
+    end
+end
