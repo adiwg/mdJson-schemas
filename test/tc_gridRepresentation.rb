0e8463e61516d9a550766329e7069c6ce8c0aac4
Josh Bradley <jbradley@arcticlcc.org>
2017-01-24 19:11:41 -0800

    Support gridRepresentation, dimension
    See #77, #105

---
+++ test/tc_gridRepresentation.rb
@@ -0,0 +1,9 @@
+require 'helper.rb'
+
+class TestGridRepresentation < TestHelper
+    def test_gridRepresentation
+        errors = JSON::Validator.fully_validate('gridRepresentation.json', @@example + 'gridRepresentation.json', strict: @@strict, list: true)
+        assert(errors.empty?, errors.join("
+"))
+    end
+end
