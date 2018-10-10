cc4888d0560543a32c3ac01a924f9810f6ff65af
Josh Bradley <jbradley@arcticlcc.org>
2018-10-10 12:06:30 -0800

    Move local to projection
    Resolves #157.

--- lib/adiwg/mdjson_schemas/version.rb
+++ lib/adiwg/mdjson_schemas/version.rb
@@ -1,6 +1,6 @@
 module ADIWG
     module MdjsonSchemas
         # Current schema version number
-        VERSION = "2.5.0"
+        VERSION = "2.5.1"
     end
 end
--- test/tc_projection.rb
+++ test/tc_projection.rb
@@ -19,7 +19,7 @@ class TestProjection < TestHelper
     def test_mapGridSystem
         codes = ADIWG::Mdcodes::getCodelistDetail('adiwg_mapGridSystem')["codelist"]
         data = codes.map do |c|
-            {:gridIdentifier => {:identifier => c["code"], :namespace => "org.adiwg.code.mapGridSystem"}}
+            {:gridIdentifier => {:identifier => c["code"], :namespace => "org.adiwg.code.mapGridSystem"}, :projectionIdentifier => {:identifier => "identifier"}}
         end
         errors = JSON::Validator.fully_validate('projection.json', data, strict: @@strict, list: true)
         assert(errors.empty?, errors.join("\n"))
